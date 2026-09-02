ecd58127281f26aef6a87acb5fca4cd20784eff4
Yechann Kim <dpcks9677@naver.com>
2026-09-02 11:11:21 +0900

    feat: 플레이어 이동 구현

---
+++ Assets/02 Scripts/Player/PlayerMove.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    // 목적 : 키보드 입력에 따라 플레이어 이동처리
+    // Update 함수는 특별한 명시가 없다면 최대한 많이 실행한다
+    private void Update()
+    {
+        /*
+        이동 구현 패턴
+        1. 입력을 받는다
+        2. 입력에 따라 방향을 구한다
+        3. 방향과 속도에 따라 이동한다.
+        */
+
+        // 입력 받기
+        float h = Input.GetAxisRaw("Horizontal"); // 왼/오른쪽 입력 상태에 따라 -1f ~ 1f 사이 값 반환
+        float v = Input.GetAxisRaw("Vertical"); // 위/아래 입력 상태에 따라 -1f ~ 1f 사이 값 반환
+        
+        Debug.Log($"h:{h}, v:{v}");
+        
+        // 방향 구하기
+        Vector2 direction = new Vector2(h, v);
+
+        Vector2 normalizedSpeed = (direction * speed).normalized; // 벡터의 길이를 1로 만든다 (정규화)
+        
+        //이동 구현
+        transform.Translate(direction * normalizedSpeed * Time.deltaTime);
+    }
+}
