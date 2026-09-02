0f61948d739ee20cf6f98f8fa7853e2bdb736897
Yechann Kim <dpcks9677@naver.com>
2026-09-02 15:59:15 +0900

    feat: 발사 기능 및 자동 발사 기능 추가

--- Assets/02 Scripts/Player/PlayerFire.cs
+++ Assets/02 Scripts/Player/PlayerFire.cs
@@ -4,14 +4,75 @@ public class PlayerFire : MonoBehaviour
 {
 
     public GameObject BulletPrefab;
-    public Transform firePoint;
+    public GameObject subBulletPrefab;
+    
+    public Transform mainFirePointLeft;
+    public Transform mainFirePointRight;
+    
+    public Transform subFirePointLeft;
+    public Transform subFirePointRight;
+
+    public float bulletCooldown = 0.5f;
+    private float fireTimer = 0.0f;
+    
+    private bool isAutoToggled = false;
+    
     // 스페이스바를 누를 때마다 총알 생성
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        fireTimer += Time.deltaTime;
+        ToggleAutoFire();
+        FireBullet();
+    }
+
+    private void FireBullet()
+    {
+        if (isAutoToggled)
         {
-            GameObject bullet = Instantiate(BulletPrefab);
-            bullet.transform.position = this.firePoint.position;
+            if(fireTimer >= bulletCooldown)
+            {
+                LoadBullet();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && fireTimer >= bulletCooldown)
+            {
+                LoadBullet();
+            }
+        }
+        
+    }
+
+    private void LoadBullet()
+    {
+        GameObject leftBullet = Instantiate(BulletPrefab);
+        GameObject rightBullet = Instantiate(BulletPrefab);
+                
+        GameObject subLeftBullet = Instantiate(subBulletPrefab);
+        GameObject subRightBullet = Instantiate(subBulletPrefab);
+                
+        leftBullet.transform.position = this.mainFirePointLeft.position;
+        rightBullet.transform.position = this.mainFirePointRight.position;
+                
+        subLeftBullet.transform.position = this.subFirePointLeft.position;
+        subRightBullet.transform.position = this.subFirePointRight.position;
+                
+        fireTimer = 0.0f;
+    }
+
+    private void ToggleAutoFire()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isAutoToggled == true)
+            {
+                isAutoToggled = false;
+            }
+            else
+            {
+                isAutoToggled = true;
+            }
         }
     }
 }
