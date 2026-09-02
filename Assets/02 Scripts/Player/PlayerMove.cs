c7f3c15a87c6b9d35d0a5267a5410d59fc2e00f6
Yechann Kim <dpcks9677@naver.com>
2026-09-02 12:53:37 +0900

    feat: 속도 증감 기능 추가
    - Q : 속도 증가 / E : 속도 감소

--- Assets/02 Scripts/Player/PlayerMove.cs
+++ Assets/02 Scripts/Player/PlayerMove.cs
@@ -1,14 +1,25 @@
+using System;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public Vector2 spawnPoint = new Vector2(0f, -2.5f);
+    
     public float speed = 5.0f;
+    public float currentSpeed = 0;
+    public float speedChangeVal = 3.0f;
+    
     public float maxHeightTop = 0f;
     public float maxHeightBottom = -5.0f;
     public float maxWidth = 2.4f;
     
     // 목적 : 키보드 입력에 따라 플레이어 이동처리
     // Update 함수는 특별한 명시가 없다면 최대한 많이 실행한다
+    private void Start()
+    {
+        transform.position = spawnPoint;
+    }
+
     private void Update()
     {
         /*
@@ -22,17 +33,28 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxisRaw("Horizontal"); // 왼/오른쪽 입력 상태에 따라 -1f ~ 1f 사이 값 반환
         float v = Input.GetAxisRaw("Vertical"); // 위/아래 입력 상태에 따라 -1f ~ 1f 사이 값 반환
         
+        
+        
         //Debug.Log($"h:{h}, v:{v}");
         
         // 방향 구하기
-        Vector2 direction = new Vector2(h, v).normalized;
-
-        Vector2 normalizedSpeed = (direction * speed); // 벡터의 길이를 1로 만든다 (정규화)
+        currentSpeed = speed;
+        Vector2 direction = new Vector2(h, v).normalized;// 벡터의 길이를 1로 만든다 (정규화)
+        
+        //속도 결정
+        if (Input.GetKey(KeyCode.Q))
+        {
+            currentSpeed = speed + speedChangeVal;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            currentSpeed = speed - speedChangeVal;
+        }
         
         //이동 구현 + Y값 제한
         if (transform.position.y <= 0 && transform.position.y >= -5)
         {
-            transform.Translate(direction * speed * Time.deltaTime);
+            transform.Translate(direction * currentSpeed * Time.deltaTime);
         }
         else if (transform.position.y > 0)
         {
@@ -52,7 +74,5 @@ public class PlayerMove : MonoBehaviour
         {
             transform.position = new Vector2(maxWidth, transform.position.y);
         }
-        
-        //속도 상승/하락
     }
 }
