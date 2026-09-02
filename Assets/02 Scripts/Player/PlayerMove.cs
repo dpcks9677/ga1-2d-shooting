8c1d6c63817cebc39890639b478b6b86610abc40
Yechann Kim <dpcks9677@naver.com>
2026-09-02 13:56:35 +0900

    refactor: 이동, 속도 조절 기능 메서드화

--- Assets/02 Scripts/Player/PlayerMove.cs
+++ Assets/02 Scripts/Player/PlayerMove.cs
@@ -22,35 +22,28 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        Move();
+    }
+
+    private void Move()
+    {
         /*
         이동 구현 패턴
         1. 입력을 받는다
         2. 입력에 따라 방향을 구한다
         3. 방향과 속도에 따라 이동한다.
         */
-
+        
         // 입력 받기
         float h = Input.GetAxisRaw("Horizontal"); // 왼/오른쪽 입력 상태에 따라 -1f ~ 1f 사이 값 반환
         float v = Input.GetAxisRaw("Vertical"); // 위/아래 입력 상태에 따라 -1f ~ 1f 사이 값 반환
-        
-        
-        
-        //Debug.Log($"h:{h}, v:{v}");
         
         // 방향 구하기
         currentSpeed = speed;
         Vector2 direction = new Vector2(h, v).normalized;// 벡터의 길이를 1로 만든다 (정규화)
         
-        //속도 결정
-        if (Input.GetKey(KeyCode.Q))
-        {
-            currentSpeed = speed + speedChangeVal;
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            currentSpeed = speed - speedChangeVal;
-        }
-        
+        ChangeSpeed();
+
         //이동 구현 + Y값 제한
         if (transform.position.y <= 0 && transform.position.y >= -5)
         {
@@ -73,6 +66,19 @@ public class PlayerMove : MonoBehaviour
         else if (transform.position.x <= -2.4)
         {
             transform.position = new Vector2(maxWidth, transform.position.y);
+        }
+    }
+
+    private void ChangeSpeed()
+    {
+        //속도 결정
+        if (Input.GetKey(KeyCode.Q))
+        {
+            currentSpeed = speed + speedChangeVal;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            currentSpeed = speed - speedChangeVal;
         }
     }
 }
