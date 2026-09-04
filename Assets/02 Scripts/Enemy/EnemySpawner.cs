740a092d6f2b53db7ebb8a705ec0de85ca058741
Yechann Kim <dpcks9677@naver.com>
2026-09-04 13:56:05 +0900

    feat: 적 스포너 랜덤화 진행

--- Assets/02 Scripts/Enemy/EnemySpawner.cs
+++ Assets/02 Scripts/Enemy/EnemySpawner.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [Header("스폰 간격")] [SerializeField] private float _spawnInterval = 3f;
-    private float _timer;
+    [SerializeField] private float _spawnInterval = 3f;
+    [SerializeField] private Enemy[] _enemyPrefabs;
 
-    [Header("스폰할 프리팹")] [SerializeField] private Enemy _enemyPrefab;
+    private float _timer;
 
     private void Update()
     {
@@ -14,17 +14,16 @@ public class EnemySpawner : MonoBehaviour
         if (_timer >= _spawnInterval)
         {
             _timer = 0f;
-
             _spawnInterval = Random.Range(1f, 3f);
-            int randomInt = Random.Range(1, 3);
-
             Spawn();
         }
     }
 
     private void Spawn()
     {
-        Enemy enemy = Instantiate(_enemyPrefab);
+        int randomIndex = Random.Range(0, _enemyPrefabs.Length);
+        Enemy enemy = Instantiate(_enemyPrefabs[randomIndex]);
+
         enemy.transform.position = transform.position;
     }
 }
