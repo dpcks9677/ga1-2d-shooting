ab166701a4cf83f7872482e5c06e832fad539225
Yechann Kim <dpcks9677@naver.com>
2026-09-10 13:15:11 +0900

    feat: 점수 레이아웃 구현
    - ScoreManager 추가 및 데이터 관리
    - 점수 UI 설정

---
+++ Assets/02 Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    // 관리: 특정 데이터에 대한 무결성과 추가, 수정, 삭제 등과 관련된 로직을 말함
+
+    private int _bestScore;
+    private int _currentScore;
+
+    // UI 책임 추가
+    [SerializeField] private TextMeshProUGUI _bestScoreText;
+    [SerializeField] private TextMeshProUGUI _currentScoreText;
+
+    private void Update()
+    {
+        _bestScoreText.text = $"Best Score: {_bestScore}";
+        _currentScoreText.text = $"Score: {_currentScore}";
+    }
+}
