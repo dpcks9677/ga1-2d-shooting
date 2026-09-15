63a9a72e7428a288eb5fb80e5fcba27ddd44ea7a
Yechann Kim <dpcks9677@naver.com>
2026-09-15 11:09:08 +0900

    feat: 세이브 데이터 json으로 저장
    - json 확장자 사용

--- Assets/02 Scripts/Upgrade/UpgradeManager.cs
+++ Assets/02 Scripts/Upgrade/UpgradeManager.cs
@@ -67,20 +67,31 @@ public class UpgradeManager : MonoBehaviour
     private void Save()
     {
         // 데이터 저장은 유의미한 정보만 저장한다. -> 레벨만 저장한다
+        UpgradeSaveData saveData = new UpgradeSaveData(_upgrades.Length);
         for (int i = 0; i < _upgrades.Length; i++)
         {
-            PlayerPrefs.SetInt($"Upgrade.{i}.Level", _upgrades[i].Level);
+            saveData.Name[i] = _upgrades[i].Name;
+            saveData.Level[i] = _upgrades[i].Level;
         }
 
+        // json 포맷으로 문자열 변환
+
+        string json = JsonUtility.ToJson(saveData);
+        PlayerPrefs.SetString("UpgradeSaveData", json);
         PlayerPrefs.Save();
     }
 
     private void Load()
     {
+        if (!PlayerPrefs.HasKey("UpgradeSaveData")) return;
+
+        string json = PlayerPrefs.GetString("UpgradeSaveData", string.Empty);
+        UpgradeSaveData saveData = JsonUtility.FromJson<UpgradeSaveData>(json);
+
         for (int i = 0; i < _upgrades.Length; i++)
         {
-            int level = PlayerPrefs.GetInt($"Upgrade.{i}.Level", 1);
-            _upgrades[i].SetLevel(level);
+            Debug.Log($"{_upgrades[i].Name} 로드 완료");
+            _upgrades[i].SetLevel(saveData.Level[i]);
         }
     }
 }
